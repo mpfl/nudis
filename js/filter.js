a80c07019e340054b579ce22bd72698ba068f823
Matthias Liffers <m@tthi.as>
2022-02-18 16:34:48 +0800

    Can I get all filter values together?
    Signed-off-by: Matthias Liffers <m@tthi.as>

--- js/filter.js
+++ js/filter.js
@@ -1,20 +1,26 @@
 
 function init() {
-    var colour_select = document.getElementById("colour_select");
-    colour_select.addEventListener('change', function() { hideShowThumbs() }, false);
+    var filters = document.getElementsByClass("filter");
+    for ( i = 0; i < filters.length; i++) {
+        filters[i].addEventListener('change', function() { hideShowThumbs() }, false);        
+    }
 }
 
 function hideShowThumbs() {
-    var colour_select = document.getElementById("colour_select");
-    var colour = colour_select.options[colour_select.selectedIndex].value;
-    if ( colour == "" ) {
+    var filters = document.getElementsByClass("filter");
+    var allFilters = "";
+    for (i = 0; i < filters.length; i++) {
+        allFilters = allFilters + filters[i].options[colour_select.selectedIndex].value + " ";
+    }
+    console.log(allFilters);
+    if ( allFilters == "" ) {
         thumbsToShow = document.getElementsByClassName("thumb");
         for ( i = 0; i < thumbsToShow.length; i++) {
             thumbsToShow[i].style.display = "inline";
         }
     } else {
         var allThumbs = document.getElementsByClassName("thumb");
-        var thumbsToShow = document.getElementsByClassName(colour);
+        var thumbsToShow = document.getElementsByClassName(allFilters);
         for ( i = 0; i < allThumbs.length; i++) {
             allThumbs[i].style.display = "none";
         }
