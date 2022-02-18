7102c3e56bf8fe5efb8066c4ba68243d4e585110
Matthias Liffers <m@tthi.as>
2022-02-18 16:26:08 +0800

    Let's unhide them too
    Signed-off-by: Matthias Liffers <m@tthi.as>

--- js/filter.js
+++ js/filter.js
@@ -7,14 +7,20 @@ function init() {
 function hideShowThumbs() {
     var colour_select = document.getElementById("colour_select");
     var colour = colour_select.options[colour_select.selectedIndex].value;
-    var allThumbs = document.getElementsByClassName("thumb");
-    var thumbsToShow = document.getElementsByClassName(colour);
-    console.log(allThumbs.length);
-    for ( i = 0; i < allThumbs.length; i++) {
-        allThumbs[i].style.display = "none";
-    }
-    for ( i = 0; i < thumbsToShow.length; i++) {
-        thumbsToShow[i].style.display = "inline";
+    if ( colour == "all" ) {
+        thumbsToShow = document.getElementsByClassName("thumb");
+        for ( i = 0; i < thumbsToShow.length; i++) {
+            thumbsToShow[i].style.display = "inline";
+        }
+    } else {
+        var allThumbs = document.getElementsByClassName("thumb");
+        var thumbsToShow = document.getElementsByClassName(colour);
+        for ( i = 0; i < allThumbs.length; i++) {
+            allThumbs[i].style.display = "none";
+        }
+        for ( i = 0; i < thumbsToShow.length; i++) {
+            thumbsToShow[i].style.display = "inline";
+        }
     }
 }
 
