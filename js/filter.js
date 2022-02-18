5884ad7bd4f6c17b766427a0b0e15932519ab0cd
Matthias Liffers <m@tthi.as>
2022-02-18 16:57:04 +0800

    Minor refactor
    Signed-off-by: Matthias Liffers <m@tthi.as>

--- js/filter.js
+++ js/filter.js
@@ -2,11 +2,32 @@
 function init() {
     var filters = document.getElementsByClassName("filter");
     for ( i = 0; i < filters.length; i++) {
-        filters[i].addEventListener('change', function() { hideShowThumbs() }, false);        
+        filters[i].addEventListener('change', function() { hideThumbs() }, false);        
     }
 }
 
-function hideShowThumbs() {
+function showAllThumbs() {
+    thumbsToShow = document.getElementsByClassName("thumb");
+    for ( i = 0; i < thumbsToShow.length; i++) {
+        thumbsToShow[i].style.display = "inline";
+    }
+}
+
+function hideAllThumbs() {
+    var allThumbs = document.getElementsByClassName("thumb");
+    for ( i = 0; i < allThumbs.length; i++) {
+        allThumbs[i].style.display = "none";
+    }
+}
+
+function showTheseThumbs(classes) {
+    var thumbsToShow = document.getElementsByClassName(classes);
+    for ( i = 0; i < thumbsToShow.length; i++) {
+        thumbsToShow[i].style.display = "inline";
+    }
+}
+
+function hideThumbs() {
     var filters = document.getElementsByClassName("filter");
     var allFilters = "";
     for (i = 0; i < filters.length; i++) {
@@ -16,19 +37,10 @@ function hideShowThumbs() {
     }
     console.log(allFilters);
     if ( allFilters == "" ) {
-        thumbsToShow = document.getElementsByClassName("thumb");
-        for ( i = 0; i < thumbsToShow.length; i++) {
-            thumbsToShow[i].style.display = "inline";
-        }
+        showAllThumbs();
     } else {
-        var allThumbs = document.getElementsByClassName("thumb");
-        var thumbsToShow = document.getElementsByClassName(allFilters);
-        for ( i = 0; i < allThumbs.length; i++) {
-            allThumbs[i].style.display = "none";
-        }
-        for ( i = 0; i < thumbsToShow.length; i++) {
-            thumbsToShow[i].style.display = "inline";
-        }
+        hideAllThumbs();
+        showTheseThumbs(allFilters);
     }
 }
 
