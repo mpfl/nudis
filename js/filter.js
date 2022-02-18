6c9df2f56b850c8e0bf22887b6eba7726584f7c9
Matthias Liffers <m@tthi.as>
2022-02-18 16:01:30 +0800

    Messin around
    Signed-off-by: Matthias Liffers <m@tthi.as>

--- js/filter.js
+++ js/filter.js
@@ -8,9 +8,7 @@ function hideShowThumbs() {
     var colour_select = document.getElementById("colour_select");
     var colour = colour_select.options[colour_select.selectedIndex].value;
     var thumbs = document.getElementsByClassName("thumb");
-    thumbs.forEach(element => {
-        element.style.display = "none";
-    });
+    console.log(thumbs.length);
 }
 
 init();
